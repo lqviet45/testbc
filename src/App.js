858cb479908a4be4c30b5dfbf8c2e1a2cbcc35bd
lqviet45 <lqviet455@gmail.com>
2023-12-11 12:01:56 +0700

    Add Azure MSAL packages and implement authentication

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
 import logo from './logo.svg';
 import './App.css';
+import { loginRequest } from './authConfig.js';
+import { useMsal } from '@azure/msal-react';
+import { PageLayout } from './PageLayout.js';
 
 function App() {
+  const { instance, accounts } = useMsal();
+  function handleLogin() {
+    instance.acquireTokenSilent({
+      ...loginRequest,
+      account: accounts[0]
+    }).then((response) => {
+      console.log(response);
+    }).catch((e) => {
+      console.log(e);
+    });
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <PageLayout>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+          <button onClick={() => handleLogin()}>
+            Acquire Token Silently
+          </button>
+        </header>
+      </div >
+    </PageLayout>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -3,11 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { MsalProvider } from '@azure/msal-react';
+import { msalConfig } from './authConfig';
+import { PublicClientApplication } from '@azure/msal-browser';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const msalInstance = new PublicClientApplication(msalConfig);
 root.render(
   <React.StrictMode>
-    <App />
+    <MsalProvider instance={msalInstance}>
+      <App />
+    </MsalProvider>
   </React.StrictMode>
 );
 
